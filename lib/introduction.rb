4244a6d4497980aee072c5530c06661927bf494f
Marty Warfel <marthawarfel@gmail.com>
2020-11-22 02:10:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,12 +7,20 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+introduction("Marty")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Marty", "Ruby")
+
 
 def introduction_with_language_optional(name, language = "Ruby")
   puts "hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional("Marty")
+introduction_with_language_optional("Marty", "Ruby")
 
 
 
